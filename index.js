f64794b93ae37e1eed82c0ce2f5a9796f6d87f0e
Harsh641-1 <187022749+Harsh641-1@users.noreply.github.com>
2025-10-20 09:15:12 +0530

    Add fuction to resize grid

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
 const container = document.querySelector('.container')
 
-function createGrid(){
-    for (let i = 0; i < 16; i++){
+
+function createGrid(numberOfSquare = 16){
+    for (let i = 0; i < numberOfSquare; i++){
         const grid = document.createElement('div')
         grid.classList.add('grid')
         container.appendChild(grid)
 
 
-        for (let i = 0; i < 16; i++){
+        for (let i = 0; i < numberOfSquare; i++){
             const item = document.createElement('div')
             item.classList.add('item')
             grid.appendChild(item)
     }
 
     }
-}
-createGrid()
+    const items = document.querySelectorAll('.item')
 
-const items = document.querySelectorAll('.item')
-
-items.forEach((item) => {
+    items.forEach((item) => {
     item.addEventListener('mouseenter', () => {
         item.style.backgroundColor = "#ee6c4d"
     })
 })
 
 
+}
+
+function resize() {
+    removeGrid()
+    let numberOfSquare = Number(prompt('Enter the number of squares per side: '))
+   
+    createGrid(numberOfSquare)
+}
+
+function removeGrid(){
+    const items = document.querySelectorAll('.item')
+    const grids = document.querySelectorAll('.grid')
+    items.forEach((item) => {
+        item.remove()
+    })
+    grids.forEach((grid) => {
+        grid.remove()
+    })
+}
+
+createGrid()
+
+const button = document.querySelector('button')
+button.addEventListener('click', resize)
